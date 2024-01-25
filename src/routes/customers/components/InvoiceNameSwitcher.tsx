7581c446a6d549b07f79869fb4b8e91b5baf2bc3
yorocomv <ishihara@gyokuroen.co.jp>
2024-01-25 16:36:41 +0900

    パンダのプロパティー値に変数を渡していたのをやめた

--- src/routes/customers/components/InvoiceNameSwitcher.tsx
+++ src/routes/customers/components/InvoiceNameSwitcher.tsx
@@ -5,38 +5,61 @@ interface InvoiceNameSwitcherProps {
 }
 
 export default function InvoiceNameSwitcher({ invoiceType }: InvoiceNameSwitcherProps): JSX.Element {
-  let text: string;
-  //   https://kagesai.net/css-marker-design
-  let color: string;
   switch (invoiceType) {
     case 1:
-      text = '伝票区分が未設定';
-      color = '#ce9eff';
-      break;
+      return (
+        <span
+          data-invoice-type={invoiceType}
+          className={css({
+            borderBottom: '.175rem solid #ce9eff',
+          })}
+        >
+          伝票区分が未設定
+        </span>
+      );
     case 2:
-      text = '仮伝';
-      color = '#7fff7f';
-      break;
+      return (
+        <span
+          data-invoice-type={invoiceType}
+          className={css({
+            borderBottom: '.175rem solid #7fff7f',
+          })}
+        >
+          仮伝
+        </span>
+      );
     case 3:
-      text = '本伝 (SMILE)';
-      color = '#ff7f7f';
-      break;
+      return (
+        <span
+          data-invoice-type={invoiceType}
+          className={css({
+            borderBottom: '.175rem solid #ff7f7f',
+          })}
+        >
+          本伝 (SMILE)
+        </span>
+      );
     case 4:
-      text = '専用伝票';
-      color = '#ffbf7f';
-      break;
+      return (
+        <span
+          data-invoice-type={invoiceType}
+          className={css({
+            borderBottom: '.175rem solid #ffbf7f',
+          })}
+        >
+          専用伝票
+        </span>
+      );
     default:
-      text = '伝票区分が古いようです';
-      color = '#7f00ff';
+      return (
+        <span
+          data-invoice-type={invoiceType}
+          className={css({
+            borderBottom: '.175rem solid #7f00ff',
+          })}
+        >
+          伝票区分が古いようです
+        </span>
+      );
   }
-  return (
-    <span
-      data-invoice-type={invoiceType}
-      className={css({
-        borderBottom: `.175rem solid ${color}`,
-      })}
-    >
-      {text}
-    </span>
-  );
 }
