075d0061ea6d4d126d1190489c4eff7191a74d5f
yorocomv <ishihara@gyokuroen.co.jp>
2024-03-08 17:05:57 +0900

    react-hook-form 関連実装途中

--- src/routes/customers/RegisterCustomer.tsx
+++ src/routes/customers/RegisterCustomer.tsx
@@ -1,6 +1,5 @@
 import { css } from '../../../styled-system/css';
 import CustomerInputs from './components/CustomerInputs';
-import Button from './components/elements/Button';
 import FormContainer from './components/elements/FormContainer';
 
 export default function RegisterCustomer() {
@@ -19,21 +18,6 @@ export default function RegisterCustomer() {
       </h2>
       <FormContainer>
         <CustomerInputs />
-        <div
-          className={css({
-            mt: 4,
-          })}
-        >
-          <Button>登録</Button>
-          <Button
-            variant="redo"
-            className={css({
-              ml: 1,
-            })}
-          >
-            クリア
-          </Button>
-        </div>
       </FormContainer>
     </>
   );
---
+++ src/routes/customers/components/elementSwitchers/FormErrorMessage.tsx
@@ -0,0 +1,30 @@
+import { FaHandPointUp } from 'react-icons/fa';
+import { css } from '../../../../../styled-system/css';
+
+interface FormErrorMessageProps {
+  message: string | undefined;
+}
+
+export default function FormErrorMessage({ message }: FormErrorMessageProps): JSX.Element | null {
+  if (message) {
+    return (
+      <p
+        className={`validation-error ${css({
+          display: 'flex',
+          alignItems: 'center',
+        })}`}
+      >
+        <FaHandPointUp
+          className={css({
+            display: 'inline-block',
+            mr: '0.25rem',
+            color: 'amber.400',
+            filter: 'drop-shadow(-1px 1px 0 #d97706)',
+          })}
+        />
+        <span>{message}</span>
+      </p>
+    );
+  }
+  return null;
+}
