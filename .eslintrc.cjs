e5b906e7690d6b6cf61325d9428083a439540c43
TokunaKimochi <tokunakimochi@gmail.com>
2024-03-20 20:14:32 +0900

    関数コンポーネントの defaultProps は非推奨らしいので eslint-config-airbnb を上書き

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,15 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    // 関数コンポーネントの defaultProps は非推奨らしいので eslint-config-airbnb を上書き
+    // https://github.com/reactjs/rfcs/blob/createlement-rfc/text/0000-create-element-changes.md#deprecate-defaultprops-on-function-components
+    'react/require-default-props': [
+      'error',
+      {
+        forbidDefaultForRequired: true,
+        functions: 'defaultArguments',
+      },
+    ],
     'jsx-a11y/label-has-associated-control': [
       2,
       {
