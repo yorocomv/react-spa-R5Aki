1fb12258c6ca27ef5a3abcdfa4c41e778b0ae5e5
TokunaKimochi <tokunakimochi@gmail.com>
2024-04-01 01:03:12 +0900

    顧客メモのモックレスポンスを用意

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,6 +3,7 @@ import { HttpResponse, http } from 'msw';
 import customers from './dummy-data/customers.json';
 import customer from './dummy-data/customer.json';
 import invoiceTypes from './dummy-data/invoice-types.json';
+import notes from './dummy-data/notes.json';
 
 const baseURL = (import.meta.env.VITE_AXIOS_BASE_URL_IN_DEV_MODE as string) ?? 'http://localhost:3000';
 
@@ -12,4 +13,5 @@ export default [
   http.get(`${baseURL}/customers/:id/checkingOverlap`, () => HttpResponse.json(customers, { status: 200 })),
   http.put(`${baseURL}/customers/:id`, () => HttpResponse.json(customer, { status: 200 })),
   http.get(`${baseURL}/invoice-types`, () => HttpResponse.json(invoiceTypes, { status: 200 })),
+  http.get(`${baseURL}/notes/:customerId`, () => HttpResponse.json(notes, { status: 200 })),
 ];
