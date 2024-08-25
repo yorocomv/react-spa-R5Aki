ac859d8e4361a6cefe3f0c3724e11b7a31056442
TokunaKimochi <tokunakimochi@gmail.com>
2024-08-25 17:30:05 +0900

    address-data-by-zip-code のモックを用意

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,6 +4,7 @@ import customers from './dummy-data/customers.json';
 import customer from './dummy-data/customer.json';
 import invoiceTypes from './dummy-data/invoice-types.json';
 import notes from './dummy-data/notes.json';
+import zipCode from './dummy-data/address-data-by-zip-code.json';
 
 const baseURL = (import.meta.env.VITE_AXIOS_BASE_URL_IN_DEV_MODE as string) ?? 'http://localhost:3000';
 
@@ -14,4 +15,5 @@ export default [
   http.put(`${baseURL}/customers/:id`, () => HttpResponse.json(customer, { status: 200 })),
   http.get(`${baseURL}/invoice-types`, () => HttpResponse.json(invoiceTypes, { status: 200 })),
   http.get(`${baseURL}/notes/:customerId`, () => HttpResponse.json(notes, { status: 200 })),
+  http.get(`${baseURL}/address-data-by-zip-code`, () => HttpResponse.json(zipCode, { status: 200 })),
 ];
