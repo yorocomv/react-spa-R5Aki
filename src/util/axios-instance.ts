c4ae91dd91bc06b3abd9e4fb1b56214134efffa4
yorocomv <ishihara@gyokuroen.co.jp>
2024-09-13 17:14:45 +0900

    個人宅かの真偽値カラム追加に対応

--- src/util/axios-instance.ts
+++ src/util/axios-instance.ts
@@ -1,21 +1,7 @@
 import axios from 'axios';
 import env from '../env';
 
-// let baseURL: string;
-// switch (import.meta.env.MODE) {
-//   case 'development':
-//   case 'msw4dev':
-//     baseURL = (import.meta.env.VITE_AXIOS_BASE_URL_IN_DEV_MODE as string) ?? 'http://localhost:3000';
-//     break;
-//   case 'production':
-//     baseURL = (import.meta.env.VITE_AXIOS_BASE_URL_IN_PROD_MODE as string) ?? 'http://192.168.1.1:3000';
-//     break;
-//   default:
-//     baseURL = 'http://localhost:80';
-// }
-
-const baseURL = `http://${env.VITE_BEP_HOST}:${env.VITE_BEP_PORT}`;
-console.log(env.SSR);
+const baseURL = `http://${env.VITE_BEP_HOST}:${env.VITE_BEP_PORT}/api`;
 
 export default axios.create({
   baseURL,
