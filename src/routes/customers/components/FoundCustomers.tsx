f749d5009ab950f9b8e01a9530891df1344bf4de
yorocomv <ishihara@gyokuroen.co.jp>
2024-10-09 15:38:55 +0900

    SearchCustomer をカスタムフック化

--- src/routes/customers/components/FoundCustomers.tsx
+++ src/routes/customers/components/FoundCustomers.tsx
@@ -1,4 +1,5 @@
 import { Link, useOutletContext } from 'react-router-dom';
+import { css } from 'styled-system/css';
 import { vstack } from '../../../../styled-system/patterns/vstack';
 import CustomerSummary from './CustomerSummary';
 import { CustomersTbRow } from '../customers.types';
@@ -10,25 +11,34 @@ export default function FoundCustomers() {
   return (
     <section className={vstack()}>
       <div>
-        <div>{latestCommunicationTime}</div>
-        {customers.length ? (
-          customers.map((customer) => (
-            <Link key={customer.id} to={`./${customer.id}`} state={customer}>
-              <CustomerSummary
-                tel={customer.tel}
-                address1={customer.address1}
-                address2={customer.address2}
-                address3={customer.address3}
-                name1={customer.name1}
-                name2={customer.name2}
-                notes={customer.notes}
-                invoice_type_id={customer.invoice_type_id}
-              />
-            </Link>
-          ))
-        ) : (
-          <div>Hit 0</div>
-        )}
+        <div
+          className={css({
+            w: 'fit-content',
+            lineHeight: 1.1,
+            marginX: 'auto',
+            marginY: '1rem',
+            px: '0.125rem',
+            fontWeight: 'bold',
+            color: 'orange.900',
+            bgColor: 'orange.200',
+          })}
+        >{`${customers.length} hit(s) / ${latestCommunicationTime} sec`}</div>
+        {customers.length
+          ? customers.map((customer) => (
+              <Link key={customer.id} to={`./${customer.id}`} state={customer}>
+                <CustomerSummary
+                  tel={customer.tel}
+                  address1={customer.address1}
+                  address2={customer.address2}
+                  address3={customer.address3}
+                  name1={customer.name1}
+                  name2={customer.name2}
+                  notes={customer.notes}
+                  invoice_type_id={customer.invoice_type_id}
+                />
+              </Link>
+            ))
+          : latestCommunicationTime !== '0' && <div>🔍️検索ワードを変えて再度お試しください🙇</div>}
       </div>
     </section>
   );
