dd68b65802f55542388c72068227773b8689b13c
yorocomv <ishihara@gyokuroen.co.jp>
2025-02-10 17:15:16 +0900

    データピッカー装飾開始

--- src/components/ui/DatePickerInput.tsx
+++ src/components/ui/DatePickerInput.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, DateInput, DatePicker, DateSegment, Group } from 'react-aria-components';
 import { CalendarDate } from '@internationalized/date';
 import { css } from 'styled-system/css';
+import { FaCalendar } from 'react-icons/fa6';
 
 export default function DatePickerInput({
   children,
@@ -16,14 +17,51 @@ export default function DatePickerInput({
       defaultValue={todayDate}
       className={css({
         display: 'flex',
-        alignItems: 'center',
+        flexDir: 'column',
+        gap: '0.5rem',
+        w: '10rem',
       })}
     >
-      <Group className={css({ display: 'flex' })}>
-        <DateInput className={css({ display: 'flex', flex: '1' })}>
-          {(segment) => <DateSegment segment={segment} />}
+      <Group
+        className={css({
+          display: 'flex',
+          alignItems: 'center',
+          color: 'slate.700',
+          bgColor: 'slate.50/90',
+          borderRadius: 'md',
+          boxShadow: 'sm',
+          pl: '0.65rem',
+          '&:where([data-rac])[data-focus-within]': { color: 'slate.950', bgColor: 'slate.50' },
+        })}
+      >
+        <DateInput className={css({ display: 'flex', flex: '1', py: '0.315rem' })}>
+          {(segment) => (
+            <DateSegment
+              segment={segment}
+              className={css({
+                fontSize: 'md',
+                lineHeight: '1.5rem',
+                px: '0.125rem',
+                my: '0.125rem',
+                borderRadius: 'sm',
+                fontVariantNumeric: 'tabular-nums',
+                outline: 'none',
+                _focus: { color: '#fff', bgColor: 'violet.700' },
+              })}
+            />
+          )}
         </DateInput>
-        <Button>▼</Button>
+        <Button
+          className={css({
+            display: 'flex',
+            alignItems: 'center',
+            outline: 'none',
+            px: '0.65rem',
+            color: 'slate.600',
+          })}
+        >
+          <FaCalendar size="1.125rem" />
+        </Button>
       </Group>
       {children}
     </DatePicker>
