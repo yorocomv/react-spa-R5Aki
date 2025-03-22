9bc54474ec8b4a0183014c052ad11779c0122ff9
TokunaKimochi <tokunakimochi@gmail.com>
2025-03-22 09:10:59 +0900

    リセットボタンに並び順のリセットを忘れていた

--- src/routes/shipping-instruction-printouts/PrintHistoryList.tsx
+++ src/routes/shipping-instruction-printouts/PrintHistoryList.tsx
@@ -89,6 +89,7 @@ export default function PrintHistoryList() {
                 setSelectCategory('printed_at');
                 setDateA(todayDate);
                 setDateB(null);
+                setIsReverse(false);
               }}
               className={css({ _hover: { cursor: 'pointer' } })}
             />
