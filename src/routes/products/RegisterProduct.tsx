613a4512a1fd018a19efa775afc95ca05d2b2494
TokunaKimochi <tokunakimochi@gmail.com>
2025-07-06 18:04:59 +0900

    通常商品とセット商品で切り替え01

--- src/routes/products/RegisterProduct.tsx
+++ src/routes/products/RegisterProduct.tsx
@@ -9,23 +9,24 @@ import FormContainer from '@/components/ui/elements/FormContainer';
 import onPromise from '@/libs/onPromise';
 import { css } from 'styled-system/css';
 
-import type { /* PostReqNewProduct, */ PostReqNewSetProduct } from './products.types';
+import type { PostReqNewProduct, PostReqNewSetProduct } from './products.types';
 
 import BasicProductFormContents from './components/BasicProductFormContents';
 import ProductCombinationsFormContents from './components/ProductCombinationsFormContents';
+import ProductComponentsFormContents from './components/ProductComponentsFormContents';
 // import ProductComponentsFormContents from './components/ProductComponentsFormContents';
 import ProductFormContents from './components/ProductFormContents';
 import ProductSkusFormContents from './components/ProductSkusFormContents';
-import { /* postReqNewProductSchema, */ postReqNewSetProductSchema } from './products.schemas';
+import { postReqNewProductSchema, postReqNewSetProductSchema } from './products.schemas';
 
-// const componentDefaultValues: PostReqNewProduct['components'][0] = {
-//   title: '',
-//   symbol: '',
-//   amount: 0,
-//   unit_type_id: 1,
-//   pieces: 1,
-//   inner_packaging_type_id: 1,
-// };
+const componentDefaultValues: PostReqNewProduct['components'][0] = {
+  title: '',
+  symbol: '',
+  amount: 0,
+  unit_type_id: 1,
+  pieces: 1,
+  inner_packaging_type_id: 1,
+};
 const combinationDefaultValues: PostReqNewSetProduct['combinations'][0] = {
   item_product_id: 0,
   quantity: 1,
@@ -33,14 +34,14 @@ const combinationDefaultValues: PostReqNewSetProduct['combinations'][0] = {
 
 export default function RegisterProduct() {
   const [isSet, setIsSet] = useState<0 | 1>(0);
-  // const methods = useForm({
-  //   mode: 'all',
-  //   resolver: zodResolver(postReqNewProductSchema),
-  //   defaultValues: {
-  //     components: [componentDefaultValues],
-  //   },
-  // });
   const methods = useForm({
+    mode: 'all',
+    resolver: zodResolver(postReqNewProductSchema),
+    defaultValues: {
+      components: [componentDefaultValues],
+    },
+  });
+  const setItemMethods = useForm({
     mode: 'all',
     resolver: zodResolver(postReqNewSetProductSchema),
     defaultValues: {
@@ -48,14 +49,21 @@ export default function RegisterProduct() {
     },
   });
 
-  const { control, handleSubmit } = methods;
-  const { fields, append, remove } = useFieldArray({
-    name: 'combinations', // 'components',
-    control,
+  const arrMethods = useFieldArray({
+    name: 'components',
+    control: methods.control,
+    rules: { minLength: 1 },
+  });
+  const setItemArrMethods = useFieldArray({
+    name: 'combinations',
+    control: setItemMethods.control,
     rules: { minLength: 1 },
   });
 
-  const onSubmit: SubmitHandler<PostReqNewSetProduct>/* <PostReqNewProduct> */ = (values) => {
+  const onSubmit: SubmitHandler<PostReqNewProduct> = (values) => {
+    console.log(values);
+  };
+  const onSubmitSetItem: SubmitHandler<PostReqNewSetProduct> = (values) => {
     console.log(values);
   };
 
@@ -72,48 +80,67 @@ export default function RegisterProduct() {
         商品新規登録
       </h2>
       <FormContainer>
-        <FormProvider {...methods}>
-          <form onSubmit={onPromise(handleSubmit(onSubmit))}>
-            <BasicProductFormContents />
-            <hr />
-            <hr />
-            <ProductFormContents isSet={isSet} setIsSet={setIsSet} drawContents={{ basic_id: true }} />
-            <hr />
-            <hr />
-            {/* {fields.map((field, index) => {
-              const isTail = index === fields.length - 1;
-              return (
-                <ProductComponentsFormContents
-                  key={field.id}
-                  index={index}
-                  remove={remove}
-                  append={append}
-                  defaultComponent={{ title: '', symbol: '', amount: 0, unit_type_id: 1, pieces: 1, inner_packaging_type_id: 1 }}
-                  isTail={isTail}
-                />
-              );
-            })} */}
-            <hr />
-            <hr />
-            {fields.map((field, index) => {
-              const isTail = index === fields.length - 1;
-              return (
-                <ProductCombinationsFormContents
-                  key={field.id}
-                  index={index}
-                  remove={remove}
-                  append={append}
-                  defaultCombination={{ item_product_id: 0, quantity: 1 }}
-                  isTail={isTail}
-                />
-              );
-            })}
-            <hr />
-            <hr />
-            <ProductSkusFormContents />
-            <Button type="submit">登録</Button>
-          </form>
-        </FormProvider>
+        {Number(isSet)
+          ? (
+              <FormProvider {...setItemMethods}>
+                <form onSubmit={onPromise(setItemMethods.handleSubmit(onSubmitSetItem))}>
+                  <BasicProductFormContents />
+                  <hr />
+                  <hr />
+                  <ProductFormContents isSet={isSet} setIsSet={setIsSet} drawContents={{ basic_id: true }} />
+                  <hr />
+                  <hr />
+                  {setItemArrMethods.fields.map((field, index) => {
+                    const isTail = index === setItemArrMethods.fields.length - 1;
+                    return (
+                      <ProductCombinationsFormContents
+                        key={field.id}
+                        index={index}
+                        remove={setItemArrMethods.remove}
+                        append={setItemArrMethods.append}
+                        defaultCombination={{ item_product_id: 0, quantity: 1 }}
+                        isTail={isTail}
+                      />
+                    );
+                  })}
+                  <hr />
+                  <hr />
+                  <ProductSkusFormContents />
+                  <Button type="submit">登録</Button>
+                </form>
+              </FormProvider>
+            )
+          : (
+              <FormProvider {...methods}>
+                <form onSubmit={onPromise(methods.handleSubmit(onSubmit))}>
+                  <BasicProductFormContents />
+                  <hr />
+                  <hr />
+                  <ProductFormContents isSet={isSet} setIsSet={setIsSet} drawContents={{ basic_id: true }} />
+                  <hr />
+                  <hr />
+                  {
+                    arrMethods.fields.map((field, index) => {
+                      const isTail = index === arrMethods.fields.length - 1;
+                      return (
+                        <ProductComponentsFormContents
+                          key={field.id}
+                          index={index}
+                          remove={arrMethods.remove}
+                          append={arrMethods.append}
+                          defaultComponent={{ title: '', symbol: '', amount: 0, unit_type_id: 1, pieces: 1, inner_packaging_type_id: 1 }}
+                          isTail={isTail}
+                        />
+                      );
+                    })
+                  }
+                  <hr />
+                  <hr />
+                  <ProductSkusFormContents />
+                  <Button type="submit">登録</Button>
+                </form>
+              </FormProvider>
+            )}
       </FormContainer>
     </>
   );
