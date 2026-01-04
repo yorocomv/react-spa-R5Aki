e9dad961638cb45e45a19025f3366b647b559650
TokunaKimochi <tokunakimochi@gmail.com>
2026-01-04 10:24:55 +0900

    環境変数の追加

--- src/env.ts
+++ src/env.ts
@@ -9,7 +9,11 @@ const envSchema = z.object({
   // `http://` or `https://` の部分は今のところハードコード
   VITE_BEP_HOST: z.union([z.string().ip(), z.literal('localhost')]),
   VITE_BEP_PORT: z.coerce.number().min(1024).max(65535),
-});
+  VITE_GS1_COMPANY_PREFIX: z.string().regex(
+    /^(45|49)(\d{5}|\d{7}|\d{8})$/,
+    '45または49で始まり、7桁・9桁・10桁の数字である必要があります',
+  ),
+}).strict();
 
 type Env = z.infer<typeof envSchema>;
 
